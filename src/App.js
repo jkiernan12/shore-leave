53749c776d8f442a719f232e3714c4786c1fba38
John Kiernan <jkiernan12@yahoo.com>
2022-01-14 06:57:40 -0500

    Create components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  // fetch('https://api.foursquare.com/v3/places/search?ll=41.8781%2C-87.6298&radius=1000&categories=13065', {
+  //   headers: {
+  //     'Authorization': 'fsq3JtsIUeGTGKP54qKENvvcQdGsJnY0NDfooAk1Nvf/bLc='
+  //   }
+  // })
+  // .then(res => res.json())
+  // .then(data => console.log(data))
+
+
+    fetch('https://api.marinas.com/v1/points/95cz')
+  //   headers: {
+  //     'Authorization': 'fsq3JtsIUeGTGKP54qKENvvcQdGsJnY0NDfooAk1Nvf/bLc='
+  //   }
+  // })
+  .then(res => res.json())
+  .then(data => console.log(data))
+
+
   return (
     <div className="App">
       <header className="App-header">

105bc372220657506ca8d8779fef00a7677c1ac3
John Kiernan <jkiernan12@yahoo.com>
2022-01-26 15:19:04 -0500

    Fix TS errors

--- src/components/NewPage/NewPage.js
+++ src/components/NewPage/NewPage.js
@@ -5,6 +5,7 @@ import Map from '../Map/Map';
 import DestinationForm from '../DestinationForm/DestinationForm'
 import MarinaListings from '../MarinaListings/MarinaListings'
 import PropTypes from 'prop-types'
+import {IMarina} from '../../interfaces'
 
 function NewPage({addTrip, checkTrip}) {
   const [marinas, setMarinas] = useState('')

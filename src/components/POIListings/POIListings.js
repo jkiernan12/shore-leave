4d3c12e544a42062da5d61f90bb146846dd45630
John Kiernan <jkiernan12@yahoo.com>
2022-01-29 15:52:57 -0500

    Add another ci tweak

--- src/components/POIListings/POIListings.js
+++ src/components/POIListings/POIListings.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import POICard from '../POICard/POICard';
 import PropTypes from 'prop-types'
-import { cleanMarinaData } from '../../utilities';
 
 function POIListings({type, POIs, selectedPOI, updateSelectedPOI, removePOI, currTrip, setCurrTrip}) {
 

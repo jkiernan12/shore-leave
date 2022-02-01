8cb84fd1bf94056174e67ab1681f174ec81474fd
John Kiernan <jkiernan12@yahoo.com>
2022-02-01 10:46:17 -0500

    Fix Nav buttons, marina date picker, auto-load POIs

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,14 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import './Form.css'
 import { Link } from 'react-router-dom';
 import { searchPOI, fetchIsochrone } from '../../api-calls.js';
 import PropTypes from 'prop-types'
 
 function Form({ setter, currTrip, setCurrTrip, setErrorMessage }) {
+  useEffect(() => {
+    handlePOIClick()
+  }, []);
 
   function handlePOIClick(e) {
-    e.preventDefault()
-    
+    if (e) {
+      e.preventDefault()
+    }
     searchPOI(currTrip.query, currTrip, setter)
     .then(data => setter(data.filter(poi => poi.travelTime <= currTrip.query.travelRadius)))
     .catch(err => setErrorMessage('There was an issue connecting with the database. Please try again later'))
--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -10,7 +10,7 @@ function MainPage({trips}) {
     <div className='MainPage'>
       <Nav />
       <TripSection title='Upcoming Trips' trips={upcomingTrips}  />
-      <TripSection title='Past Trips' trips={pastTrips}  />
+      {pastTrips.length && <TripSection title='Past Trips' trips={pastTrips}  />}
     </div>
    );
 }
